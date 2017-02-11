b91af10a532fc026e6e577ac44967401f1123c0a
joshmorel <morel.josh@hotmail.com>
2017-02-10 19:24:06 -0500

    Added bak files

---
+++ bin/bak/deskbak.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+EXCLUDE_FILE=$(dirname ${BASH_SOURCE})/deskbak-exclude.txt
+MOUNT_POINT="/mnt/"
+BACKUP_DEV="/dev/sda8"
+BACKUP_DIR="/mnt/bak/"
+
+mount $BACKUP_DEV $MOUNT_POINT
+
+rsync -aHAXvP --delete --exclude-from=$EXCLUDE_FILE /* $BACKUP_DIR
+
+umount $MOUNT_POINT
