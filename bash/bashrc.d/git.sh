f774ea5406692f9f1f1b8021d1d2499dd58446f8
Josh Morel <morel.josh@hotmail.com>
2018-04-01 10:18:31 -0400

    refactor prompt-related to separate file

--- bash/bashrc.d/git.sh
+++ bash/bashrc.d/git.sh
@@ -1,17 +1,4 @@
 # git & related environment
-
-# prompt
-parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
-if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)\[\033[00m\] \$ '
-else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch) \$ '
-fi
-unset color_prompt force_color_prompt
-
-# git aliases
 alias g-i='git init'
 alias g-s='git status'
 alias g-a.='git add .'
